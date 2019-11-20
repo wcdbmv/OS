65caba97cf59dc957599486cd90f8bbd5e12ddfc
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-11-20 19:45:21 +0300

    Add main04.c

---
+++ sem05/lab04/main04.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "display.h"
+
+#define CHILDREN_COUNT 3
+#define BUFFER_SIZE 32
+
+int pipefd[2];
+
+void on_child(int i) {
+	display_row_formatted("on child%02d", i);
+
+	// get message
+	close(pipefd[1]);
+	char buffer[BUFFER_SIZE];
+	read(pipefd[0], buffer, sizeof buffer);
+	display_row_formatted("on child%02d [receives \"%s\"]", i, buffer);
+}
+
+void send_message_to_child(int i) {
+	close(pipefd[0]);
+	char message[] = "message to childxx";
+	snprintf(message, sizeof message, "message to child%02d", i);
+	write(pipefd[1], message, sizeof message);
+}
+
+int main(void) {
+	if (pipe(pipefd) == -1) {
+		perror("pipe");
+		return EXIT_FAILURE;
+	}
+
+	display_header();
+	display_row("on parent");
+
+	for (int i = 0; i < CHILDREN_COUNT; ++i) {
+		switch (fork()) {
+		case -1:
+			perror("fork");
+			return EXIT_FAILURE;
+		case 0:
+			on_child(i);
+			return EXIT_SUCCESS;
+		}
+	}
+
+	for (int i = 0; i < CHILDREN_COUNT; ++i) {
+		send_message_to_child(i);
+
+		int status;
+		const pid_t childpid = wait(&status);
+		if (childpid == -1) {
+			perror("wait");
+			return EXIT_FAILURE;
+		}
+
+		if (WIFEXITED(status)) {
+			display_row_formatted("on parent [child%02d returns %d]", i, WEXITSTATUS(status));
+		} else {
+			display_row_formatted("on parent [child%02d terminated abnormally]", i);
+		}
+	}
+}
