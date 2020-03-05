4551898b13dfc8fb812e6483254983d0d9e2a783
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-03-06 00:30:42 +0300

    Fix smth

--- sem06/lab02/tree.c
+++ sem06/lab02/tree.c
@@ -30,24 +30,41 @@ void print_link(const char *indent, const char *branch, const char *name)
 	printf("%s%s%s%s%s\n", indent, branch, COLOR_BOLDCYAN, name, COLOR_RESET);
 }
 
+void print_lstat_error_message(const char *pathname)
+{
+	const char *errmsg = "";
+	switch (errno) {
+	case EACCES:       errmsg = "Permission denied";                     break;
+	case EFAULT:       errmsg = "Bad address";                           break;
+	case ELOOP:        errmsg = "Too many levels of symbolic links";     break;
+	case ENAMETOOLONG: errmsg = "File name too long";                    break;
+	case ENOENT:       errmsg = "No such file or directory";             break;
+	case ENOMEM:       errmsg = "Cannot allocate memory";                break;
+	case ENOTDIR:      errmsg = "Not a directory";                       break;
+	case EOVERFLOW:    errmsg = "Value too large for defined data type"; break;
+	default:           errmsg = strerror(errno);                         break;
+	}
+	fprintf(stderr, "[7] lstat(%s, &st): %s\n", pathname, errmsg);
+}
+
 int tree_process(const char *dirname, const char *indent)
 {
 	assert(dirname && indent);
 
-	// Запомним абсолютный путь директории, из которой вызвали
+	// Запомним путь директории, из которой вызвали
 	char previous_path[PATH_MAX];
 	if (getcwd(previous_path, PATH_MAX) == NULL) {
 		fprintf(stderr, "[1] getcwd(%s, %d): %s\n", previous_path, PATH_MAX, strerror(errno));
 		return EXIT_FAILURE;
 	}
 
-	// Перейдём в директорию, которую запросили
+	// Перейдём в директорию с именем dirname
 	if (chdir(dirname) == -1) {
 		fprintf(stderr, "[2] chdir(%s): %s\n", dirname, strerror(errno));
 		return EXIT_FAILURE;
 	}
 
-	// Запомним абсолютный путь этой директории
+	// Запомним путь этой директории
 	char path[PATH_MAX];
 	if (getcwd(path, PATH_MAX) == NULL) {
 		fprintf(stderr, "[3] getcwd(%s, %d): %s\n", path, PATH_MAX, strerror(errno));
@@ -56,7 +73,7 @@ int tree_process(const char *dirname, const char *indent)
 	}
 
 	// Откроем директорию по её абсолютному пути.
-	// Если использовать относительный путь dirname, то может возникнуть ошибка при ".."
+	// Если использовать короткое имя dirname, то может возникнуть ошибка при ".."
 	DIR *dir;
 	if ((dir = opendir(path)) == NULL) {
 		fprintf(stderr, "[4] opendir(%s): %s\n", path, strerror(errno));
@@ -71,23 +88,26 @@ int tree_process(const char *dirname, const char *indent)
 		return EXIT_FAILURE;
 	}
 
+	errno = 0;
+	int rc = EXIT_SUCCESS;
 	struct dirent *entry;
 	while ((entry = readdir(dir)) != NULL) {
 		if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
 			if (push_vector(entries, entry) != EXIT_SUCCESS) {
 				fprintf(stderr, "[6] push_vector: %s\n", strerror(errno));
-				delete_vector(&entries);
-				closedir(dir);
-				chdir(previous_path);
-				return EXIT_FAILURE;
+				rc = EXIT_FAILURE;
 			}
 		}
+	}
+	if (errno != 0) {
+		fprintf(stderr, "[7] readdir: %s\n", strerror(errno));
+		rc = EXIT_FAILURE;
 	}
 
 	const char *current_branch    = NULL;
 	const char *additional_indent = NULL;
 	struct stat st;
-	for (size_t i = 0; i < entries->size; ++i) {
+	for (size_t i = 0; i < entries->size && rc != EXIT_FAILURE; ++i) {
 		entry = entries->data[i];
 		if (i < entries->size - 1) {
 			current_branch    = "├── ";
@@ -102,19 +122,19 @@ int tree_process(const char *dirname, const char *indent)
 		next_indent[PATH_MAX - 1] = '\0';
 		strcat(next_indent, additional_indent);
 
-		if (entry->d_type == DT_DIR) {
-			print_dirname(indent, current_branch, entry->d_name);
-			tree_process(entry->d_name, next_indent);
-		} else {
-			if (lstat(entry->d_name, &st) == -1) {
-				fprintf(stderr, "[7] lstat(%s, &st): %s\n", path, strerror(errno));
-				delete_vector(&entries);
-				closedir(dir);
-				chdir(previous_path);
-				return EXIT_FAILURE;
-			}
+		if (lstat(entry->d_name, &st) == -1) {
+			print_lstat_error_message(entry->d_name);
+			rc = EXIT_FAILURE;
+			break;
+		}
 
-			if ((st.st_mode & S_IFMT) == S_IFLNK) {
+		if (S_ISDIR(st.st_mode)) {
+			print_dirname(indent, current_branch, entry->d_name);
+			if ((rc = tree_process(entry->d_name, next_indent)) != EXIT_SUCCESS) {
+				break;
+			}
+		} else {
+			if (S_ISLNK(st.st_mode)) {
 				print_link(indent, current_branch, entry->d_name);
 			} else if (st.st_mode & S_IXUSR) {
 				print_executable(indent, current_branch, entry->d_name);
@@ -128,16 +148,15 @@ int tree_process(const char *dirname, const char *indent)
 
 	if (closedir(dir) == -1) {
 		fprintf(stderr, "[8] closedir: %s\n", strerror(errno));
-		chdir(previous_path);
-		return EXIT_FAILURE;
+		rc = EXIT_FAILURE;
 	}
 
 	if (chdir(previous_path) == -1) {
 		fprintf(stderr, "[9] chdir(%s): %s\n", previous_path, strerror(errno));
-		return EXIT_FAILURE;
+		rc = EXIT_FAILURE;
 	}
 
-	return EXIT_SUCCESS;
+	return rc;
 }
 
 int tree(const char *root) {
