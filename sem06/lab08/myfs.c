a8192880e4bc6e11a7bc9a2eb6b2add6034041d9
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-05-05 20:19:42 +0300

    Improve myfs.c

--- sem06/lab08/myfs.c
+++ sem06/lab08/myfs.c
@@ -8,7 +8,6 @@ MODULE_AUTHOR("Kerimov A. IU7-64b");
 MODULE_DESCRIPTION("my filesystem");
 
 #define MYFS_MAGIC_NUMBER 0xDEADBEAF
-#define MYFS_CACHE_INODE_COUNT 128
 #define SLABNAME "myfs_inode_cache"
 
 struct myfs_inode {
@@ -17,32 +16,33 @@ struct myfs_inode {
 };
 
 static struct kmem_cache *myfs_inode_cachep = NULL;
-static struct myfs_inode **line = NULL;
+static struct myfs_inode **myfs_inodes = NULL;
+static int cache_inode_count = 128;
+module_param(cache_inode_count, int, 0);
 static int busy = 0;
-static int sco = 0;
 
 static struct myfs_inode *cache_get_inode(void)
 {
-	if (busy == MYFS_CACHE_INODE_COUNT) {
+	if (busy == cache_inode_count) {
 		return NULL;
 	}
 
-	return line[busy++];
+	return myfs_inodes[busy++] = kmem_cache_alloc(myfs_inode_cachep, GFP_KERNEL);
 }
 
 static struct inode *myfs_make_inode(struct super_block *sb, int mode)
 {
-	struct myfs_inode *mi = NULL;
+	struct myfs_inode *myfs_inodep = NULL;
 	struct inode *ret = new_inode(sb);
 	if (ret) {
 		inode_init_owner(ret, NULL, mode);
 		ret->i_size = PAGE_SIZE;
 		ret->i_atime = ret->i_mtime = ret->i_ctime = current_time(ret);
-		if ((mi = cache_get_inode()) != NULL) {
-			mi->i_mode = ret->i_mode;
-			mi->i_ino = ret->i_ino;
+		if ((myfs_inodep = cache_get_inode())) {
+			myfs_inodep->i_mode = ret->i_mode;
+			myfs_inodep->i_ino = ret->i_ino;
 		}
-		ret->i_private = mi;
+		ret->i_private = myfs_inodep;
 	}
 
 	return ret;
@@ -56,7 +56,7 @@ static void myfs_put_super(struct super_block *sb)
 static const struct super_operations myfs_super_ops = {
 	.put_super  = myfs_put_super,
 	.statfs     = simple_statfs,
-	.drop_inode = generic_delete_inode
+	.drop_inode = generic_delete_inode,
 };
 
 static int myfs_fill_sb(struct super_block *sb, void *data, int silent)
@@ -99,62 +99,42 @@ static struct file_system_type myfs_type = {
 	.owner   = THIS_MODULE,
 	.name    = "myfs",
 	.mount   = myfs_mount,
-	.kill_sb = kill_anon_super
+	.kill_sb = kill_anon_super,
 };
-
-
-void co(void *p)
-{
-	*(int *) p = (int) p;
-	++sco;
-}
 
 static int __init myfs_init(void)
 {
-	int i, ret;
-	if (!(line = kmalloc(sizeof (struct myfs_inode *) * MYFS_CACHE_INODE_COUNT, GFP_KERNEL))) {
-		printk(KERN_ERR "myfs: kmalloc error\n");
-		return -ENOMEM;
-	}
-	if (!(myfs_inode_cachep = kmem_cache_create(SLABNAME, sizeof (struct myfs_inode), 0, SLAB_HWCACHE_ALIGN, co))) {
-		kfree(line);
-		printk(KERN_ERR "myfs: kmem_cache_create error\n");
-		return -ENOMEM;
-	}
-	for (i = 0; i < MYFS_CACHE_INODE_COUNT; ++i) {
-		line[i] = NULL;
-		if (!(line[i] = kmem_cache_alloc(myfs_inode_cachep, GFP_KERNEL))) {
-			while (--i >= 0) {
-				kmem_cache_free(myfs_inode_cachep, line[i]);
-			}
-			kmem_cache_destroy(myfs_inode_cachep);
-			kfree(line);
-			printk(KERN_ERR "myfs: kmem_cache_alloc error\n");
-			return -ENOMEM;
-		}
-	}
-	printk(KERN_INFO "myfs: allocate %d objects into slab: %s\n", MYFS_CACHE_INODE_COUNT, SLABNAME);
-	printk(KERN_INFO "myfs: object size %lu bytes, full size %lu bytes\n", sizeof (struct myfs_inode), sizeof (struct myfs_inode) * MYFS_CACHE_INODE_COUNT);
-	printk(KERN_INFO "myfs: constructor called %d times\n", sco);
-
-	if ((ret = register_filesystem(&myfs_type))) {
+	int ret = register_filesystem(&myfs_type);
+	if (ret) {
 		printk(KERN_ERR "myfs: cannot register filesystem\n");
 		return ret;
 	}
+
+	if (!(myfs_inodes = kmalloc(sizeof(struct myfs_inode *) * cache_inode_count, GFP_KERNEL))) {
+		printk(KERN_ERR "myfs: kmalloc error\n");
+		return -ENOMEM;
+	}
+
+	if (!(myfs_inode_cachep = kmem_cache_create(SLABNAME, sizeof(struct myfs_inode), 0, SLAB_POISON, NULL))) {
+		kfree(myfs_inodes);
+		printk(KERN_ERR "myfs: kmem_cache_create error\n");
+		return -ENOMEM;
+	}
+
 	printk(KERN_DEBUG "myfs: module loaded\n");
 	return 0;
 }
 
 static void __exit myfs_exit(void)
 {
-	int i, ret;
-	for (i = 0; i < MYFS_CACHE_INODE_COUNT; ++i) {
-		kmem_cache_free(myfs_inode_cachep, line[i]);
+	int i;
+	for (i = 0; i < busy; ++i) {
+		kmem_cache_free(myfs_inode_cachep, myfs_inodes[i]);
 	}
 	kmem_cache_destroy(myfs_inode_cachep);
-	kfree(line);
+	kfree(myfs_inodes);
 
-	if ((ret = unregister_filesystem(&myfs_type))) {
+	if (unregister_filesystem(&myfs_type)) {
 		printk(KERN_ERR "myfs: cannot unregister filesystem\n");
 	}
 	printk(KERN_DEBUG "myfs: module unloaded\n");
